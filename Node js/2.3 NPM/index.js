74489ebc1693d18c27a089f9b65f8adc70313a83
DEvackemann <anshikadubey8591@gmail.com>
2024-01-27 21:30:08 +0530

    fixed : structure

---
+++ Node js/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+//var generateName = require('sillyname');
+
+import generateName from "sillyName";
+var sillyName = generateName();
+console.log(`Hello my name is ${sillyName}.`);
+
+// const superheroes = require("superheroes");
+
+import superheroes from "superheroes";
+const name = superheroes.random();
+console.log(`I am ${name}! Super Hero come to save you baby girl`);
