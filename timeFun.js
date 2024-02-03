6204bbeeb49ef07e3b3d5d96057946e6df136250
DEvackemann <150266045+DEvackemann@users.noreply.github.com>
2024-02-03 17:28:15 +0530

    fixed:timeFun.js

--- timeFun.js
+++ timeFun.js
@@ -1,9 +1,11 @@
-console.loh (navigator)
-if ("geoloaction" in navigator){
-    navigator.geoloaction.getCurrentPosition(function(position){
-        let lat =position.coords.latitude;
-        let long =position.coords.longitude;
+console.log(navigator);
+
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        console.log(`Latitude: ${lat}, Longitude: ${long}`);
     });
-}else{
-    console.log("position not found")
+} else {
+    console.log("Position not found");
 }
