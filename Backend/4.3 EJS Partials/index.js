5ef97cc87286936fb37b4ce9a56b1fb025f7b5ac
DEvackemann <anshikadubey8591@gmail.com>
2024-02-10 18:57:40 +0530

    Updated

--- Backend/4.3 EJS Partials/index.js
+++ Backend/4.3 EJS Partials/index.js
@@ -2,6 +2,23 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+app.use(express.static("public")); //used to specify the static file 
+app.get( "/" , (req , res ) => {
+res.render("index.ejs");
+});
+
+app.get( "/about" , (req , res ) => {
+  res.render("about.ejs");
+  });
+
+  app.get( "/contact" , (req , res ) => {
+    res.render("contact.ejs");
+    });
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+
 
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
@@ -10,6 +27,14 @@ Step 3: Add the routes to handle the render of the about and contact pages.
   Hint: Check the nav bar in the header.ejs to see the button hrefs
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+
+
+
+
+
+
+
+
+//npm i
+// nodemon .\index.js 
+// open localhost:3000
